def137c2b5b6e6282a902107f7b0f340cfb6fa8f
Emerson Lima <emersonlimago123@gmail.com>
2023-04-06 16:35:24 -0300

    added header

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
     include_once("templates/header.php")
 ?>
+ <main>
     <div class="purple-back"></div>
-    <divc class="green-line">    
-</div>
-</body>
+    <div class="green-line"></div>
 
-</html>
+    <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis rem impedit pariatur omnis nostrum praesentium eos dolor. Minus cupiditate exercitationem id deserunt voluptas in nobis libero culpa, placeat, consequatur dicta?
+    </h1>
+ </main>
+<?php
+    include_once("templates/footer.php")
+?>
--- templates/header.php
+++ templates/header.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL?>/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto:wght@400;700&display=swap"
@@ -17,13 +17,16 @@
 </head>
 <body>
 
-<header>
+<header class="header">
     <h1>The Blog<span class="dot">.</span></h1>
-    <div><div class="green-stripe"></div>
+    <div class="sections"><div class="green-stripe"></div>
     <p>Home</p>
     <p>Sobre</p>
     <p>Categorias</p>
     <p>Contato</p>
 </div>
-
-<header>
+<div class="input-div">
+    <input type="text" class="buscar" placeholder="Buscar">
+    <div class="div-search-icon"><img src="<?= $BASE_URL?>/images/search.svg" alt="search icon"></div>
+</div>
+</header>
