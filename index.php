250433018afc1c3aa8b51fc9f91ff9deaf6b658b
Emerson Lima <emersonlimago123@gmail.com>
2023-04-06 15:33:38 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include_once("templates/header.php")
+?>
+    <div class="purple-back"></div>
+    <divc class="green-line">    
+</div>
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,29 @@
+<?php
+    include_once("server.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+    <h1>The Blog<span class="dot">.</span></h1>
+    <div><div class="green-stripe"></div>
+    <p>Home</p>
+    <p>Sobre</p>
+    <p>Categorias</p>
+    <p>Contato</p>
+</div>
+
+<header>
