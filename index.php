90a53228a7b1c1e8d8f0e78e7e734508be9101e6
Emerson Lima <emersonlimago123@gmail.com>
2023-04-07 18:05:18 -0300

    added bottom posts

--- index.php
+++ index.php
@@ -18,6 +18,23 @@
         <img src="<?= $BASE_URL?>images/arrow-right.svg" alt="arrow icon">
     </div>
     <div class="green-line"></div>
+
+    <div class="bottom-imgs">
+            <img src="<?= $BASE_URL?>images/post-2.png" alt="post image">
+            <img src="<?= $BASE_URL?>images/post-3.png" alt="post image">
+            <img src="<?= $BASE_URL?>images/post-4.png" alt="post image">
+    </div>
+
+    <section class="bottom-posts">
+            <?php foreach($posts as $post):?>
+                <?php if($post["id"] > 4 && $post["id"] < 8):?>
+                <div class="bottom-post">
+                    <h2><?= $post["title"]?></h2>
+                    <p><?= $post["abstract"]?></p>
+                </div>
+                <?php endif;?>
+            <?php endforeach; ?>
+    </section>
  </main>
 <?php
     include_once("templates/footer.php")
