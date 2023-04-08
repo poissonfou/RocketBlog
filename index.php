1f9a690f7df6248c2dfdf70bcb8eb507f98174c1
Emerson Lima <emersonlimago123@gmail.com>
2023-04-07 21:21:06 -0300

    added middle section

--- index.php
+++ index.php
@@ -19,6 +19,30 @@
     </div>
     <div class="green-line"></div>
 
+    <section class="middle-section">
+          <div class="middle-main-section">
+            <img src="<?= $BASE_URL?>images/post-1.png" alt="">
+            <?php foreach($posts as $post):?>
+                <?php if($post["id"]  == 4):?>
+                    <p class="main-date">Janeiro 04, 2022</p>
+                    <h2><?= $post["title"]?></h2>
+                    <p><?= $post["abstract"]?></p>
+                <?php endif;?>
+            <?php endforeach;?>
+          </div>
+          <div class="middle-side-posts">
+            <?php foreach($posts as $post):?>
+                <?php if($post["id"] > 1 && $post["id"] < 4):?>
+                    <div class="middle-side-post">
+                    <p class="side-date">Janeiro 04, 2022</p>
+                    <h2><?= $post["title"]?></h2>
+                    <p><?= $post["abstract"]?></p>
+                    </div>
+                <?php endif?>
+            <?php endforeach;?>
+           </div>
+    </section>
+
     <div class="bottom-imgs">
             <img src="<?= $BASE_URL?>images/post-2.png" alt="post image">
             <img src="<?= $BASE_URL?>images/post-3.png" alt="post image">
